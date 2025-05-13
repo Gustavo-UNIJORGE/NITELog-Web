57d16da64d19c6da5babaffadf08234836a1aa17
Guilherme <gui.franklin17@gmail.com>
2025-05-13 11:15:02 -0300

    página de login

--- Página de login/login.js
+++ Página de login/login.js
@@ -1,10 +1,14 @@
-function validateFields(){
-    const emailValid=isEmailValid();
-    document.getElementById('recover-password-button').disabled=!emailValid;
-
-    const passwordValid = isPasswordValid();
-    document.getElementById("login-button").disabled=!passwordValid || !emailValid;
+function onChangeEmail(){
+   toggleEmailErrors();
+   toggleButtonsDisable();
 }
+
+function onChangePassword(){
+    toggleButtonsDisable();
+    togglePasswordErrors();
+
+}
+
 function isEmailValid(){
     const email = document.getElementById("email").value;
     if(!email){
@@ -12,6 +16,41 @@ function isEmailValid(){
     }else{
         return validateEmail(email);
     }
+}
+
+
+
+function toggleEmailErrors(){
+    const email = document.getElementById('email').value;
+    if(!email){
+        document.getElementById('email-required-error').style.display = "block"; 
+    }else{
+        document.getElementById('email-required-error').style.display = "none"; 
+    }
+
+    if(validateEmail(email)){
+          document.getElementById('email-invalid-error').style.display = "none"; 
+    }else{
+        document.getElementById('email-invalid-error').style.display = "block"; 
+    }
+
+}
+
+function togglePasswordErrors(){
+    const password = document.getElementById('password').value;
+    if(!password){
+        document.getElementById('password-required-error').style.display = "block";
+    }else{
+        document.getElementById('password-required-error').style.display = "none";
+    }
+}
+
+function toggleButtonsDisable(){
+    const emailValid=isEmailValid();
+    document.getElementById('recover-password-button').disabled=!emailValid;
+
+    const passwordValid = isPasswordValid();
+    document.getElementById("login-button").disabled=!passwordValid || !emailValid;
 }
 function isPasswordValid(){
     const password = document.getElementById("password").value;
