70ded9e53cff48755af08591aaefa3b7d02babc5
Zokyn <guttycasaes@hotmail.com>
2025-05-26 14:51:40 -0300

    feat(pages): adicionado paginas de Guilherme e React-Router

---
+++ src/Login.tsx
@@ -0,0 +1,48 @@
+import {onChangeEmail, onChangePassword} from "./Script";
+import { Link } from 'react-router-dom';
+
+const Login = ({ }) => {
+    return (
+        <form className="forms">
+
+            <div className="tituloForms">
+                <div className="acessarPortal">Acessar o Portal</div>
+                <div className="texto">Bem-Vindo de volta</div>
+            </div>
+
+            <div>
+                <div><label>Email</label></div>
+                <input type="email" onInput={onChangeEmail} name="email" id="email" placeholder="Digite seu email" className="campoEmail" />
+                <div className="error" id="email-invalid-error">Email inválido</div>
+                <div className="error" id="email-required-error">Campo obrigatório</div>
+            </div>
+
+
+            <div>
+                
+                <div className="labelSenha"><label>Senha</label></div>
+                <input type="password" id="password" name="senha" onInput={onChangePassword} placeholder="Digite sua senha" className="campoSenha" />
+                <div className="error" id="password-required-error">Campo obrigatório</div>
+                <div className="esqueciSenhaPosicao">
+                    <button type="button" className="esqueciSenha" id="recover-password-button"  disabled={true}>Esqueceu a senha?</button>
+                </div>
+                
+            </div>
+
+
+            <div>
+                <button type="button" className="entrar" id="login-button" disabled={true}>Entrar</button>
+            </div>
+
+            <div className="register">
+                
+                <span>Não tem uma conta?</span>
+                <Link to="/cadastro" className="fazerCadastro">Fazer cadastro</Link>
+            </div>
+
+        </form>
+    )
+}
+
+export default Login;
+
---
+++ src/Register.tsx
@@ -0,0 +1,51 @@
+import { Link } from 'react-router-dom';
+import {onChangeName, onChangeConfirmPassword} from './ScriptRegister';
+import {onChangeEmail, onChangePassword} from "./Script";
+
+const Register = ({ }) => {
+    return (
+        <form className="forms">
+
+            <div className="tituloForms">
+                <div className="acessarPortal">Acessar o Portal</div>
+                <div className="texto">Insira seus dados</div>
+            </div>
+
+            <div>
+                <div><label>Nome completo</label></div>
+                <input type="text" name="name" id="name" onInput={onChangeName} placeholder="Digite seu nome" className="campoNome" />
+                <div className="error" id="name-required-error">Campo obrigatório</div>
+            </div>
+
+
+
+            <div>
+                <div className="labelCadastrarEmail"><label>Email</label></div>
+                <input type="email" name="email" onInput={onChangeEmail} id="email" placeholder="Digite seu email" className="campoEmail" />
+                <div className="error" id="email-invalid-error">Email inválido</div>
+                <div className="error" id="email-required-error">Campo obrigatório</div>
+            </div>
+
+
+            <div>
+                <div className="labelCadastrarSenha"><label>Senha</label></div>
+                <input type="password" id="password" onInput={onChangePassword} name="senha" placeholder="Digite sua senha" className="campoSenha" />
+                <div className="error" id="password-required-error">Campo obrigatório</div>
+            </div>
+
+            <div>
+                <div className="labelConfirmeSenha"><label>Confirme sua senha</label></div>
+                <input type="password" id="confirmPassword" onInput={onChangeConfirmPassword} name="confirmeSenha" placeholder="Digite sua senha" className="campoSenha" />
+                <div className="error" id="confirmPassword-required-error">Campo obrigatório</div>
+                <div className="error" id="confirmPassword-invalid-error">Senha inválida</div>
+            </div>
+
+            <div className="register">
+                <Link to="/home" className="Cadastrar">Cadastrar</Link>
+            </div>
+
+        </form>
+    )
+}
+
+export default Register;
